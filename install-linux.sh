0ac4186ccf74812c3362fc986cd76ed586c35b47
n <nvnivs@gmail.com>
2024-08-21 09:44:37 +0200

    Fixes code installed check

--- install-linux.sh
+++ install-linux.sh
@@ -27,7 +27,7 @@ apt_install "apt-transport-https"
 
 # https://github.com/microsoft/vscode
 # needs: wget gpg apt-transport-https
-if [ ! $(which codeAAA) ]; then
+if [ ! $(which code) ]; then
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /tmp/packages.microsoft.gpg
     sudo install -D -o root -g root -m 644 /tmp/packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
     echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
