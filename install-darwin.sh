ad17a77f4e590dc8f8bb442997397cbce93608e2
Nuno Pereira <nvnivs@gmail.com>
2024-08-22 11:36:19 +0200

    Installs stow

--- install-darwin.sh
+++ install-darwin.sh
@@ -13,6 +13,9 @@ brew install bat
 # https://github.com/jqlang/jq
 brew install jq
 
+# https://www.gnu.org/software/stow/
+brew install stow
+
 # https://github.com/gnachman/iTerm2
 brew install --cask iterm2
 
