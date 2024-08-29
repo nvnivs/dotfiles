342909183f84423a8211a9ffc996b9efa11c4eca
Nuno Pereira <nvnivs@gmail.com>
2024-08-29 14:35:06 +0200

    Uses brew bundle

--- install-darwin.sh
+++ install-darwin.sh
@@ -7,17 +7,8 @@ if [[ ! $(which brew) ]]
     else brew update
 fi
 
-# https://github.com/sharkdp/bat
-brew install bat
+# Install brew bundle
+brew bundle install
 
-# https://github.com/jqlang/jq
-brew install jq
-
-# https://www.gnu.org/software/stow/
-brew install stow
-
-# https://github.com/gnachman/iTerm2
-brew install --cask iterm2
-
-# https://github.com/microsoft/vscode
-brew install --cask visual-studio-code
+# Remove outdated versions from the cellar.
+brew cleanup
