e3c450ffb249964149ed05606336d2647e55280b
n <nvnivs@gmail.com>
2024-09-16 11:19:16 +0200

    Installs chrome

--- install-linux.sh
+++ install-linux.sh
@@ -36,7 +36,13 @@ sudo install -D -o root -g root -m 644 /tmp/packages.microsoft.gpg /etc/apt/keyr
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
 rm -f /tmp/packages.microsoft.gpg
 sudo apt update
-sudo apt install code
+sudo apt install -y code
+
+# https://www.digitalocean.com/community/tutorials/install-chrome-on-linux-mint
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo add-apt-repository "deb http://dl.google.com/linux/chrome/deb/ stable main"
+sudo apt update
+sudo apt install -y google-chrome-stable
 
 # https://github.com/casey/just
 rm -rf ~/.local/bin/just
